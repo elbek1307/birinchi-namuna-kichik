f4cfbdddef3b068698b56c9b5f336435e06e6c1e
elbek1307 <elbekjonabdushukurov1307@gmail.com>
2026-07-03 15:43:37 +0500

    add subcrition deyarli bitdi

--- src/pages/AddSubscription/AddSubscription.jsx
+++ src/pages/AddSubscription/AddSubscription.jsx
@@ -1,21 +1,71 @@
-import React from 'react'
+import React from "react";
+import MainLayout from "../../layouts/MainLayout";
+import { useState } from "react";
 
 const AddSubscription = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [status, setStatus] = useState("Active");
+  const [subscriptions, setSubscriptions] = useState(data);
+
+
+  const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const newSubscription = {
+    name,
+    price,
+    status,
+  };
+
+  setSubscriptions([
+  ...subscriptions,
+  {
+    id: Date.now(),
+    name,
+    price,
+    status,
+  },
+]);
+};
+
   return (
-     <>
-        <h1>Add Subscription</h1>
+    <MainLayout>
+      <h1>Add Subscription</h1>
 
-        <input type="text" />
-        <br />
-        <input type="text" />
-        <br />
-        <select name="" id="">
-            <option value=""></option>
-        </select>
-        <br />
-        <button >save</button>
-     </>
-  )
-}
+      <form onSubmit={handleSubmit}>
+        <input
+        type="text"
+        placeholder="Subscription name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
-export default AddSubscription
+      <br />
+      <br />
+
+      <input
+        type="number"
+        placeholder="Price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <select value={status} onChange={(e) => setStatus(e.target.value)}>
+        <option value="Active">Active</option>
+        <option value="Paused">Paused</option>
+      </select>
+
+      <br />
+      <br />
+
+      <button type="submit">Save</button>
+      </form>
+    </MainLayout>
+  );
+};
+
+export default AddSubscription;
